e5734c458d753d67a627f4cf66ffabed92d1cfba
swatikadam-123 <swatikadam907@gmail.com>
2022-09-26 09:38:13 +0530

    added: display default value all data type

---
+++ ClassDemo.java
@@ -0,0 +1,26 @@
+package javahello;
+
+public class ClassDemo {
+  static boolean val1;
+  static int   val2;
+  static float val3;
+  static double val4;
+  static long   val5;
+  static String val6;
+  static Short val7;
+  static byte val8;
+  
+	public static void main(String[] args) {
+		System.out.println("Default values");
+		System.out.println("val1 ="+ val1);
+		System.out.println("val2 ="+ val2);
+		System.out.println("val3 ="+ val3);
+        System.out.println("val4 ="+ val4);
+        System.out.println("val5 ="+ val5);
+        System.out.println("val6 ="+ val6);
+        System.out.println("val7 ="+ val7);
+        System.out.println("val8 ="+ val8);
+       
+	}
+
+}
