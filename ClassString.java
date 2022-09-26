c17be64092da15108b04f5f86d56d15bdfd943d1
swatikadam-123 <swatikadam907@gmail.com>
2022-09-26 09:55:32 +0530

    added: check two strings equal or not

---
+++ ClassString.java
@@ -0,0 +1,14 @@
+package javahello;
+
+public class ClassString {
+
+	public static void main(String[] args) {
+		String mystr1="hello";
+		String mystr2="hi";
+		boolean retval1=mystr2.equals(mystr1);
+		System.out.println("mystr2.equals mystr1=" +retval1 );
+		//System.out.println("mystr1.equals(mystr2)");
+        
+	}
+
+}
