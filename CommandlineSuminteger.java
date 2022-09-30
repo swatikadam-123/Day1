fe4b58744aaa87cd57f3bcd4f2e2aef8c940c237
swatikadam-123 <swatikadam907@gmail.com>
2022-09-30 21:45:23 +0530

    added: sum of two integer using commandline

---
+++ CommandlineSuminteger.java
@@ -0,0 +1,19 @@
+package javahello;
+
+import java.util.Scanner;
+
+public class CommandlineSuminteger {
+
+	public static void main(String[] args) {
+		  int a,b,sum;
+		 Scanner s =new Scanner(System.in);
+		  a=s.nextInt();
+		  b=s.nextInt();
+		System.out.println("enter first valu");
+		System.out.println("enter second valu");
+	    sum=a+b;
+		System.out.println("Sum is "+sum);
+	}
+	}
+
+
